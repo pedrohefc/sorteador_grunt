9bbeb25936855cb03f7498feeb081401990a9fb8
Pedro Costa <pedrohefc@gmail.com>
2024-12-07 20:12:24 -0300

    ajuste

--- gruntfile.js
+++ gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
                         {
                             match:'ENDEREÇO_DO_CSS',
                             replacement:'./styles/main.min.css'
-                        }
+                        },
                         {
                             match:'ENDEREÇO_DO_JS',
                             replacement:'.scripts/main.min.js'
